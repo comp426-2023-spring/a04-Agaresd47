23aed247812f61be343593d5e2efc2432df5d988
Zifei Dong <agares@ROG-Z590>
2023-04-12 18:19:59 -0400

    finish try

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.get('/app/rpsls/', (req, res) => {
 })
 
 app.get('/app/rps/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
 })
 
 app.get('/app/rpsls/play', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
 })
 
 app.post('/app/rps/play', (req, res) => {
