68fb21dc08059c7cf2c78702dd421d7d2784ccdc
Zifei Dong <agares@ROG-Z590>
2023-04-12 17:48:18 -0400

    finish

---
+++ server.js
@@ -0,0 +1,11 @@
+import {rps, rpsls} from "./lib/rpsls.js"
+import minimist from 'minimist'
+import express from 'express'
+
+var argv = minimist(process.argv.slice(2));
+const port = argv.port || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
